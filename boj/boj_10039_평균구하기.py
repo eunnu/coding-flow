6aa3222cc1df7f9d6b920317f059404514e576c2
eunnu <angellina9413@gmail.com>
2022-07-23 23:31:18 +0900

    boj 1789 수들의 합

--- boj/boj_10039_평균구하기.py
+++ boj/boj_10039_평균구하기.py
@@ -1,9 +1,15 @@
-sum = 0
-for i in range(5):
+# sum = 0
+# for i in range(5):
     
-    a = int(input())
-    if a < 40:
-         a = 40
-    sum += a
+#     a = int(input())
+#     if a < 40:
+#          a = 40
+#     sum += a
 
-print(int(sum/5))
+# print(int(sum/5))
+
+
+num = int(input())
+while(num):
+    print(num%10)
+    num=num//10
---
+++ boj/boj_1789_수들의합.py
@@ -0,0 +1,8 @@
+s = int(input())
+n = 1
+sum_n = 0
+while sum_n <= s:
+    sum_n += n
+    n += 1
+
+print(n-2)
