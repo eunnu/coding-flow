929cd154a70272ceaad4838a4fa657ca6c28fd96
eunnu <angellina9413@gmail.com>
2022-08-05 23:22:52 +0900

    swea 9480 py

--- swea/swea_9480.py
+++ swea/swea_9480.py
@@ -4,32 +4,40 @@ T = int(input())
 for tc in range(1, T + 1):
     N = int(input())
 
-    sam = ''
-    sam_li = []
-    alpha = {'a' : 0, 'b' : 0, 'c' : 0, 'd' : 0, 'e' : 0, 'f' : 0, 'g' : 0, 'h' : 0, 'i' : 0, 'j' : 0, 'k' : 0, 'l' : 0, 'm' : 0,
-                'n' : 0, 'p' : 0, 'q' : 0, 'r' : 0, 's' : 0, 't' : 0, 'u' : 0, 'v' : 0, 'w' : 0, 'x' : 0, 'y' : 0, 'z' : 0}
+    word = []
     for _ in range(N):
-        sam = input()
-        sam_li.append(sam)
-            
-    for i in range(N):
-        for j in range(len(sam_li[i])):
-            if sam_li[i][j] in alpha:
-                alpha[sam_li[i][j]] += 1
-                
+        temp = input()
+        word.append(temp)
+
+    # def combi(arr, r, combi_li):
+    #     arr = sorted(arr)
+    #     li = []
+    #     def generate(chosen):
+    #         if len(chosen) == r:
+    #             li.append(chosen)
+    #             combi_li.append(li)
+    #             return 
+
+    #         start = arr.index(chosen[-1]) + 1 if chosen else 0
+    #         for nxt in range(start, len(arr)):
+    #             chosen.append(arr[nxt])
+    #             generate(chosen)
+    #             chosen.pop()
+    #     generate(li)
+
+    combi_li = [] # 모든 조합을 넣어줄 리스트
+    for i in range(1,N+1):
+        combi_li += list(itertools.combinations(word, i))
+
+    res = 0 
     
-    print(alpha)
-    '''
-    alpha_li = []
-    for i in alpha.keys():
-        if (alpha.get(i) - 1):
-            alpha_li.append(i)
-
-    ans = 0
-    result = list(itertools.combinations((alpha_li),2))
-
-    print(result)
-    #print(f'#{tc} ')
-    '''
-            
-
+    for i in combi_li:
+        temp =  ''
+        for j in i:
+            temp += j
+            # 조합 단어를 모두 더해 중복되는 알파벳을 전부 지움
+            temp_num = len(set(temp))
+            if temp_num == 26: #길이가 알파벳이면
+                res += 1 #결과를 더해준다.
+                break        
+    print(f'#{tc} {res}')
